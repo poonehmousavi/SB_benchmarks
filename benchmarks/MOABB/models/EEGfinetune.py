2cede16253b19d072cdf06798a1463c3c8b787be
Jama Hussein Mohamud <engmubarak48@gmail.com>
2024-05-04 17:40:33 -0400

    rename the file and the model

--- benchmarks/MOABB/models/EEGfinetune.py
+++ benchmarks/MOABB/models/EEGfinetune.py
@@ -1,8 +1,4 @@
 import torch
-import speechbrain as sb
-from speechbrain.lobes.models.Xvector import Xvector, Classifier
-
-import torch
 from transformers import Wav2Vec2Config, Wav2Vec2FeatureExtractor, Wav2Vec2Model
 
 
@@ -123,6 +119,7 @@ class Wav2Vec2ForFinetuning(torch.nn.Module):
         outputs = self.model(features["input_values"])
         return outputs.last_hidden_state
 
+
 class AttentionMLP(torch.nn.Module):
     def __init__(self, input_dim, hidden_dim):
         super(AttentionMLP, self).__init__()
@@ -136,17 +133,17 @@ class AttentionMLP(torch.nn.Module):
         x = self.layers(x)
         att_w = torch.nn.functional.softmax(x, dim=2)
         return att_w
-    
 
-class CustomModel(torch.nn.Module):
-    def __init__(self, ssl_model,attention_mlp, x_vector,classifier):
-        super(CustomModel, self).__init__()
+
+class eegFinetuneModel(torch.nn.Module):
+    def __init__(self, ssl_model, attention_mlp, x_vector, classifier):
+        super(eegFinetuneModel, self).__init__()
         self.ssl_model = ssl_model
         self.attention_mlp = attention_mlp
         self.x_vector = x_vector
         self.classifier = classifier
-    
-    def forward(self,x):
+
+    def forward(self, x):
         """Returns the output of the model.
 
         Arguments
@@ -154,9 +151,9 @@ class CustomModel(torch.nn.Module):
         x : torch.Tensor (batch, time, EEG channel, channel)
             Input to convolve. 4d tensors are expected.
         """
-        bs, c, t, _ =x.size()
-        feats = self.ssl_model(x.permute(0,2,1,3).reshape(bs*c,t))
-        feats = feats.reshape(bs,c,-1, feats.shape[-1]).permute(0,2,1,3)
+        bs, c, t, _ = x.size()
+        feats = self.ssl_model(x.permute(0, 2, 1, 3).reshape(bs * c, t))
+        feats = feats.reshape(bs, c, -1, feats.shape[-1]).permute(0, 2, 1, 3)
         att_w = self.attention_mlp(feats)
         feats = torch.matmul(att_w.transpose(2, -1), x).squeeze(-2)
         h = self.x_vector(feats).squeeze(1)
