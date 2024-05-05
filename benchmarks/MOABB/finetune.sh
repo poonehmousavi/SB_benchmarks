cc5da6d6dc69bc27bcb005cef8e03befcd7de21b
Jama Hussein Mohamud <engmubarak48@gmail.com>
2024-05-05 13:23:17 -0400

    minor update on localpath

--- benchmarks/MOABB/finetune.sh
+++ benchmarks/MOABB/finetune.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 python train.py hparams/MotorImagery/BNCI2014001/ssl.yaml \
-    --data_folder '/data/BNCI2014001' \
-    --cached_data_folder '/data' \
-    --output_folder '/results/single-fold-example-braindecode/BNCI2014001' \
+    --data_folder 'data/BNCI2014001' \
+    --cached_data_folder 'data' \
+    --output_folder 'results/single-fold-example-braindecode/BNCI2014001' \
     --data_iterator_name 'leave-one-session-out' \
     --target_subject_idx 0 \
     --target_session_idx 1 \
