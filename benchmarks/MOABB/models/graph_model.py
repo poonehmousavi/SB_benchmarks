cde4b31e87e6cc466fd9be8969d4d45fc67e1667
Jama Hussein Mohamud <engmubarak48@gmail.com>
2024-06-25 16:58:07 -0400

    removed GraphSelfAttention and added pool layer to STGNN model

--- benchmarks/MOABB/models/graph_model.py
+++ benchmarks/MOABB/models/graph_model.py
@@ -5,86 +5,7 @@ import torch
 import torch.nn.functional as F
 import torch_geometric
 from torch_geometric.nn import GCNConv, GIN, global_mean_pool
-from torch_geometric_temporal.nn.recurrent import GConvGRU
-from torch.nn import MultiheadAttention
 import speechbrain as sb
-
-
-class GraphSelfAttention(torch.nn.Module):
-    """GraphSelfAttention.
-
-    Arguments
-    ---------
-    input_shape: tuple
-        The shape of the input.
-    dense_n_neurons: int
-        Output layer shape
-    activation_type: str
-        Activation function of the hidden layers.
-
-    Example
-    -------
-    >>> inp_tensor = torch.rand([1, 200, 32, 1])
-    >>> model = GraphSelfAttention(input_shape=inp_tensor.shape)
-    >>> output = model(node_fts, edge_index)
-    >>> output.shape
-    torch.Size([1,4])
-    """
-
-    def __init__(
-        self,
-        input_shape=None,  # (B, N, T)
-        dense_n_neurons=4,
-        activation_type="relu",
-        embed_dim=768,
-        num_heads=8
-    ):
-        super().__init__()
-        if input_shape is None:
-            raise ValueError("Must specify input_shape")
-        
-        if activation_type == "gelu":
-            self.activation = torch.nn.GELU()
-        elif activation_type == "elu":
-            self.activation = torch.nn.ELU()
-        elif activation_type == "relu":
-            self.activation = torch.nn.ReLU()
-
-        self.gnn_conv = GCNConv(input_shape[1], embed_dim)
-        self.global_pool = global_mean_pool
-
-        self.self_attention = MultiheadAttention(embed_dim=embed_dim, num_heads=num_heads, batch_first=True)
-
-        self.dense_module = torch.nn.Sequential(
-            torch.nn.Linear(embed_dim, embed_dim),
-            self.activation,
-            torch.nn.Linear(embed_dim, dense_n_neurons),
-            torch.nn.LogSoftmax(dim=1)
-        )
-
-    def forward(self, graph):
-        """Returns the output of the model.
-
-        Arguments
-        ---------
-        x : torch.Tensor (B, N, T)
-            Input to GNN. 3D tensors are expected. 
-        edge_index : torch.Tensor
-            Edge indices for GNN.
-        batch : torch.Tensor
-            Batch vector for GNN.
-        """
-        x = self.gnn_conv(graph.x, graph.edge_index)
-        x = self.activation(x)
-        x = self.global_pool(x, graph.batch)  # (B, embed_dim)
-        
-        x = x.unsqueeze(1)  # (B, 1, embed_dim)
-        attn_output, _ = self.self_attention(x, x, x)  # Self-attention
-        
-        x = attn_output.squeeze(1)  # (B, embed_dim)
-        x = self.dense_module(x)
-        
-        return x
 
 
 class STGNN(torch.nn.Module):
@@ -234,12 +155,23 @@ class STGNN(torch.nn.Module):
                 affine=True,
             ),
         )
+        self.rep_conv_module.add_module(
+            "pool_0",
+            sb.nnet.pooling.Pooling2d(
+                pool_type=cnn_pool_type,
+                kernel_size=cnn_temporal_pool,
+                stride=cnn_temporal_pool,
+                pool_axis=[1, 2],
+            ),
+        )
         self.rep_conv_module.add_module("act_0", self.activation)
         self.rep_conv_module.add_module("dropout_0", torch.nn.Dropout(p=dropout))
         self.rep_conv_module.add_module("flatten_0", torch.nn.Flatten())
 
         # Shape of intermediate feature maps
-        out = self.rep_conv_module(torch.ones((1, embed_dim, 1, 1)))
+        edge_index = torch.empty((2, 0), dtype=torch.long)
+        gnn_out = self.spatial_gnn(node_fts, edge_index)
+        out = self.rep_conv_module(gnn_out.unsqueeze(-1).unsqueeze(-1))
         in_channels = out.shape[-1]
         self.dense_module = torch.nn.Sequential(
             torch.nn.Linear(in_channels, dense_n_neurons),
@@ -259,6 +191,7 @@ class STGNN(torch.nn.Module):
             Batch vector for GNN.
         """
         x = graph.x.unsqueeze(-1).unsqueeze(-1)
+        assert x.shape[1] == self.T, f"Inconsistency, unexpect size of T: {self.T} != {x.shape[1]}"
         x = self.temp_conv_module(x)
         x = self.spatial_gnn(x, graph.edge_index)
         x = x.unsqueeze(-1).unsqueeze(-1)
