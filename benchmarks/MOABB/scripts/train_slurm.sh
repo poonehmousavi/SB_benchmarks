f40a6ea83ce0b3a5d0c09aac13eb003d5fd46d84
Drew Wagner <drewarthurwagner@gmail.com>
2024-07-03 14:09:44 -0400

    init train_slurm script

---
+++ benchmarks/MOABB/scripts/train_slurm.sh
@@ -0,0 +1,11 @@
+module load StdEnv/2020
+module load python/3.10.2
+module load scipy-stack
+
+virtualenv $SLURM_TMPDIR/venv
+source $SLURM_TMPDIR/venv/bin/activate
+
+pip install --no-index $SCRATCH/wheels/*
+
+cd $HOME/speechbrain-benchmarks-private/benchmarks/MOABB
+pip install --no-index -r extra_requirements.txt
