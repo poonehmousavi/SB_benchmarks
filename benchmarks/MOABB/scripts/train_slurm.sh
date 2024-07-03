1888bfa94a92fa4d1832d5751b1bf978b85b3b0f
Drew Wagner <drewarthurwagner@gmail.com>
2024-07-03 14:51:10 -0400

    update script

--- benchmarks/MOABB/scripts/train_slurm.sh
+++ benchmarks/MOABB/scripts/train_slurm.sh
@@ -5,8 +5,15 @@ module load scipy-stack
 virtualenv --no-download $SLURM_TMPDIR/venv
 source $SLURM_TMPDIR/venv/bin/activate
 
-pip install --no-index $SCRATCH/wheels/*
-pip install --no-index h5py orion scikit-learn torch_geometric torchinfo
+pip install --no-index "h5py<4.0.0" scikit-learn orion torch_geometric torchinfo
+pip install --no-index $SCRATCH/wheels/EDFlib_Python-1.0.8-py3-none-any.whl
+pip install --no-index $SCRATCH/wheels/edfio-0.4.3-py3-none-any.whl
+pip install --no-index $SCRATCH/wheels/mne-1.7.1-py3-none-any.whl
+pip install --no-index $SCRATCH/wheels/mne_bids-0.15.0-py3-none-any.whl
+pip install --no-index $SCRATCH/wheels/pymatreader-0.0.32-py3-none-any.whl
+pip install --no-index $SCRATCH/wheels/pyriemann-0.6-py2.py3-none-any.whl
+pip install --no-index $SCRATCH/wheels/speechbrain-1.0.0-py3-none-any.whl
+pip install --no-index --no-deps $SCRATCH/wheels/moabb-1.1.0-py3-none-any.whl
 
 
 cd $HOME/speechbrain-benchmarks-private/benchmarks/MOABB
