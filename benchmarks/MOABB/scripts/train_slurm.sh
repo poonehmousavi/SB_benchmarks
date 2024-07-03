3948676f839f30229725aee9aa7fe52fe5d5dd80
Drew Wagner <drewarthurwagner@gmail.com>
2024-07-03 15:22:32 -0400

    memory profiler req

--- benchmarks/MOABB/scripts/train_slurm.sh
+++ benchmarks/MOABB/scripts/train_slurm.sh
@@ -14,6 +14,7 @@ pip install --no-index $SCRATCH/wheels/mne_bids-0.15.0-py3-none-any.whl
 pip install --no-index $SCRATCH/wheels/pymatreader-0.0.32-py3-none-any.whl
 pip install --no-index $SCRATCH/wheels/pyriemann-0.6-py2.py3-none-any.whl
 pip install --no-index $SCRATCH/wheels/speechbrain-1.0.0-py3-none-any.whl
+pip install --no-index $SCRATCH/wheels/memory_profiler-0.61.0-py3-none-any.whl
 pip install --no-index $SCRATCH/wheels/moabb-1.0.0-py3-none-any.whl
 
 
