5537299a7ec514ee345959d6c6d682d6fa034d0d
Drew Wagner <drewarthurwagner@gmail.com>
2024-07-03 15:57:21 -0400

    fix cd

--- benchmarks/MOABB/scripts/slurm_train_base.sh
+++ benchmarks/MOABB/scripts/slurm_train_base.sh
@@ -3,4 +3,5 @@ source $HOME/speechbrain-benchmarks-private/benchmarks/MOABB/scripts/slurm_insta
 mkdir -p $SLURM_TMPDIR/mne_data
 tar -axf $HOME/projects/def-ravanelm/datasets-open/${dataset_tag}.tar -C $SLURM_TMPDIR/mne_data
 
+cd $HOME/speechbrain-benchmarks-private/benchmarks/MOABB
 python train.py $@ --data_folder=$SLURM_TMPDIR/mne_data --cached_data_folder=$SLURM_TMPDIR/mne_data/pkl
