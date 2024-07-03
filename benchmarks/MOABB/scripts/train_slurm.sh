26e1de182df973f3d4dfdc48884970fa9041b00c
Drew Wagner <drewarthurwagner@gmail.com>
2024-07-03 14:28:17 -0400

    install h5py manually

--- benchmarks/MOABB/scripts/train_slurm.sh
+++ benchmarks/MOABB/scripts/train_slurm.sh
@@ -6,6 +6,7 @@ virtualenv --no-download $SLURM_TMPDIR/venv
 source $SLURM_TMPDIR/venv/bin/activate
 
 pip install --no-index $SCRATCH/wheels/*
+pip install --no-index h5py
 
 cd $HOME/speechbrain-benchmarks-private/benchmarks/MOABB
 pip install --no-index -r extra-requirements.txt
