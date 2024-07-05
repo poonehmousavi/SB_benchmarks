9489c13dab1e70f320d34351e73c8beb424b63dc
Drew Wagner <drewarthurwagner@gmail.com>
2024-07-05 17:10:34 -0400

    module load

--- benchmarks/MOABB/scripts/slurm_hparam_opt.sh
+++ benchmarks/MOABB/scripts/slurm_hparam_opt.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
-source $HOME/speechbrain-benchmarks-private/benchmarks/MOABB/scripts/slurm_install_env.sh
+$HOME/speechbrain-benchmarks-private/benchmarks/MOABB/scripts/slurm_install_env.sh
+module load StdEnv/2020
+module load python/3.10.2
+module load scipy-stack
+
 source $SLURM_TMPDIR/venv/bin/activate
 $HOME/speechbrain-benchmarks-private/benchmarks/MOABB/scripts/slurm_cp_dataset.sh $dataset_tag
 
