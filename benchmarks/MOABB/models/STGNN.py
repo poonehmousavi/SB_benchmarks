050fb0dc545d616e0b5e974863caf3af683ef2eb
Jama Hussein Mohamud <engmubarak48@gmail.com>
2024-07-05 00:39:26 -0400

    add STGNNI model

--- benchmarks/MOABB/models/STGNN.py
+++ benchmarks/MOABB/models/STGNN.py
@@ -1,9 +1,22 @@
 
 import torch
-import torch.nn.functional as F
+import torch.nn as nn
 import torch_geometric
 from torch_geometric.nn import GCNConv, global_mean_pool
 import speechbrain as sb
+
+
+class PositionEncoder(torch.nn.Module):
+    def __init__(self, in_dim, out_dim, activation=torch.nn.ReLU()):
+        super().__init__()
+        self.mlp = torch.nn.Sequential(
+            torch.nn.Linear(in_dim, out_dim),
+            activation,
+            torch.nn.Linear(out_dim, out_dim)
+        )
+
+    def forward(self, pos):
+        return self.mlp(pos)
 
 
 class STGNN(torch.nn.Module):
@@ -195,3 +208,89 @@ class STGNN(torch.nn.Module):
     def _get_temp_size(self, node_fts):
         _, T1, _, T2 = node_fts.shape
         return T1 * T2
+
+
+class STGNNI(nn.Module):
+    def __init__(
+        self,
+        input_shape,
+        cnn_kernels,
+        cnn_kernelsize,
+        cnn_pool,
+        cnn_pool_type="avg",
+        dropout=0.5,
+        embed_dim=768,
+        dense_n_neurons=4,
+        activation_type="relu",
+    ):
+        super().__init__()
+        if input_shape is None:
+            raise ValueError("Must specify input_shape")
+        
+        # Activation function
+        if activation_type == "gelu":
+            self.activation = nn.GELU()
+        elif activation_type == "elu":
+            self.activation = nn.ELU()
+        elif activation_type == "relu":
+            self.activation = nn.ReLU()
+
+        self.T = input_shape[1]
+
+        # Temporal convolutional module
+        self.conv_module = nn.Sequential(
+            nn.Conv2d(1, cnn_kernels[0], kernel_size=(cnn_kernelsize[0], 1), padding='same'),
+            nn.BatchNorm2d(cnn_kernels[0], momentum=0.01, affine=True),
+            # nn.AvgPool2d(kernel_size=(cnn_pool[0], 1), stride=(cnn_pool[0], 1)) if cnn_pool_type == "avg" else nn.MaxPool2d(kernel_size=(cnn_pool[0], 1), stride=(cnn_pool[0], 1)),
+            self.activation,
+            nn.Dropout(p=dropout),
+
+            nn.Conv2d(cnn_kernels[0], cnn_kernels[1], kernel_size=(cnn_kernelsize[1], 1)),
+            nn.BatchNorm2d(cnn_kernels[1], momentum=0.01, affine=True),
+            nn.AvgPool2d(kernel_size=(cnn_pool[0], 1), stride=(cnn_pool[0], 1)) if cnn_pool_type == "avg" else nn.MaxPool2d(kernel_size=(cnn_pool[0], 1), stride=(cnn_pool[0], 1)),
+            self.activation,
+            nn.Dropout(p=dropout),
+
+            nn.Conv2d(cnn_kernels[1], cnn_kernels[2], kernel_size=(cnn_kernelsize[2], 1)),
+            nn.BatchNorm2d(cnn_kernels[2], momentum=0.01, affine=True),
+            nn.AvgPool2d(kernel_size=(cnn_pool[1], 1), stride=(cnn_pool[1], 1)) if cnn_pool_type == "avg" else nn.MaxPool2d(kernel_size=(cnn_pool[1], 1), stride=(cnn_pool[1], 1)),
+            self.activation,
+            nn.Dropout(p=dropout),
+        )
+
+        # Position encoder
+        self.pos_encoder = PositionEncoder(in_dim=3, out_dim=cnn_kernels[2], activation=self.activation)
+        self.flatten = nn.Flatten()
+
+        # Calculate the output size of the convolutional module
+        conv_out = self.conv_module(torch.zeros(1, 1, self.T, 1))
+        self.new_T = conv_out.size(2)
+        conv_out_size = self.flatten(conv_out).shape[1]
+        # Spatial Graph convolution
+        self.spatial_gnn = torch_geometric.nn.Sequential('x, edge_index', [
+            (GCNConv(conv_out_size, embed_dim), 'x, edge_index -> x'),
+            (self.activation, 'x -> x'),
+            (GCNConv(embed_dim, embed_dim), 'x, edge_index -> x'),
+            (self.activation, 'x -> x'),
+        ])
+
+        self.global_pool = global_mean_pool
+
+        # Dense layer
+        self.dense_module = nn.Sequential(
+            nn.Linear(embed_dim, dense_n_neurons),
+            nn.LogSoftmax(dim=1)
+        )
+
+    def forward(self, graph):
+        assert graph.x.shape[1] == self.T, f"Inconsistency, unexpected size of T: {self.T}"
+        assert len(graph.x.shape) == 3, f"feature dim should be 3, got: {graph.x.shape}"
+        x = graph.x.unsqueeze(1)  # (N, 1, T, 1)
+        pos_encoding = self.pos_encoder(graph.pos)  # (N, D)
+        pos_encoding = pos_encoding.unsqueeze(-1).repeat(1, 1, self.new_T)  # (N, D, T')
+        x = self.conv_module(x).squeeze()  # (N, D, T')
+        x = self.flatten((x + pos_encoding))  # (N, D)
+        x = self.spatial_gnn(x, graph.edge_index)  # (N, D)
+        x = self.global_pool(x, graph.batch)  # (B, D)
+        x = self.dense_module(x)  # (B, classes)
+        return x
