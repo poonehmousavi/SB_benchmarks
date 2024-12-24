4729007d099d3d474c4a47f89602ed64a18214f0
poonehmousavi <moosavi.pooneh@gmail.com>
2024-12-23 22:44:08 -0500

    fix

--- benchmarks/DASB/LibriSpeech/extraction/extract.py
+++ benchmarks/DASB/LibriSpeech/extraction/extract.py
@@ -93,6 +93,7 @@ if __name__ == "__main__":
         logger.info(f"Saving embeddings ...")
         tokens_extractor.save_pretrained_embeddings(
             (save_folder / "embeddings").as_posix(),
+            hparams["vocab_size"],
             hparams["num_codebooks"],
-            hparams["vocab_size"]
+           
         )
