da79205c4c68753a500ad4bdeaf1accc9352ac04
poonehmousavi <moosavi.pooneh@gmail.com>
2025-01-04 12:39:49 -0500

    add iemocao extraction

---
+++ benchmarks/DASB/IEMOCAP/extraction/extract.py
@@ -0,0 +1,90 @@
+#!/usr/bin/env/python3
+"""Recipe for extracting a discrete tokens with IEMOCAP.
+
+Authors
+ * Pooneh Mousavi 2024
+"""
+
+import os
+import sys
+import logging
+import pathlib as pl
+import speechbrain as sb
+from speechbrain.dataio.dataset import DynamicItemDataset
+from speechbrain.utils.distributed import run_on_main
+from hyperpyyaml import load_hyperpyyaml
+
+base_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), "../../"))
+sys.path.append(base_dir)
+
+print(base_dir)
+
+logger = logging.getLogger(__name__)
+
+
+if __name__ == "__main__":
+    # CLI:
+    hparams_file, run_opts, overrides = sb.parse_arguments(sys.argv[1:])
+    with open(hparams_file) as fin:
+        hparams = load_hyperpyyaml(fin, overrides)
+
+    # Create experiment directory
+    sb.create_experiment_directory(
+        experiment_directory=hparams["output_folder"],
+        hyperparams_to_save=hparams_file,
+        overrides=overrides,
+    )
+
+    from iemocap_prepare import prepare_data  # noqa E402
+
+    # Data preparation, to be run on only one process.
+    if not hparams["skip_prep"]:
+        sb.utils.distributed.run_on_main(
+            prepare_data,
+            kwargs={
+                "data_original": hparams["data_folder"],
+                "save_json_train": hparams["train_annotation"],
+                "save_json_valid": hparams["valid_annotation"],
+                "save_json_test": hparams["test_annotation"],
+                "split_ratio": [80, 10, 10],
+                "different_speakers": hparams["different_speakers"],
+                "test_spk_id": hparams["test_spk_id"],
+                "seed": hparams["seed"],
+            }
+        )
+
+    tokens_extractor = hparams["tokens_extractor"]
+    data_folder = hparams["data_folder"]
+    datasets = []
+    for split in ["train", "valid","test"]:
+        train_annotation
+        json_path = hparams[f"{split}_annotation"]
+        name = pl.Path(json_path).stem
+        dataset = sb.dataio.dataset.DynamicItemDataset.from_json(
+            json_path=json_path, replacements={"data_root": data_folder},
+        )
+        datasets.append(dataset)
+
+    merged_data = {
+        key: value
+        for dataset in datasets
+        for key, value in dataset.data.items()
+    }
+    merged_dataset = DynamicItemDataset(merged_data)
+
+    save_folder = pl.Path(hparams["save_folder"])
+    logger.info("Extracting dataset tokens ...")
+    tokens_extractor.extract_tokens(
+        merged_dataset,
+        hparams["num_codebooks"],
+        (save_folder / "librispeech").as_posix(),
+    )
+
+    if hparams["save_embedding"]:
+        save_folder = pl.Path(hparams["save_folder"])
+        logger.info(f"Saving embeddings ...")
+        tokens_extractor.save_pretrained_embeddings(
+            (save_folder / "embeddings").as_posix(),
+            vocab_size=hparams["vocab_size"],
+            num_codebooks=hparams["num_codebooks"],
+        )
