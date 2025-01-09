dd7f69581a6a59a76f2d9e1cac70091f306e5d04
poonehmousavi <moosavi.pooneh@gmail.com>
2025-01-09 17:47:11 -0500

    report acc instead of error

--- benchmarks/DASB/utils/aggregate_results.py
+++ benchmarks/DASB/utils/aggregate_results.py
@@ -144,6 +144,6 @@ if __name__ == "__main__":
 
     # Report final metric to Orion
     # Remember: orion expects metrics to be minimized!
-    if eval_metric == "acc" or eval_metric == "f1":
+    if eval_metric == "error_rate" or eval_metric == "f1":
         final_metric = 1 - final_metric
     report_objective(final_metric)
