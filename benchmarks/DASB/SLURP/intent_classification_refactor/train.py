6674ba6a8f6c5c1b9ebfa7460739be00b38a4a01
poonehmousavi <mousavi.pooneh@gmail.com>
2025-01-18 21:33:44 +0000

    add slurp

---
+++ benchmarks/DASB/SLURP/intent_classification_refactor/train.py
@@ -0,0 +1,377 @@
+#!/usr/bin/env/python3
+"""Recipe for "direct" (speech -> scenario) "Intent" classification using SLURP Dataset.
+18 Scenarios classes are present in SLURP (calendar, email)
+We encode input waveforms into features using a discrete tokens.
+The probing is done using either a  RNN layer or time-pooling and followed by a linear classifier.
+
+Authors
+ * Pooneh Mousavi 2024
+"""
+import os
+import sys
+import time
+import torchaudio
+import logging
+from hyperpyyaml import load_hyperpyyaml
+import speechbrain as sb
+from speechbrain.utils.distributed import run_on_main
+import torch
+
+base_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), "../../"))
+sys.path.append(base_dir)
+
+
+logger = logging.getLogger(__name__)
+
+class IntentIdBrain(sb.Brain):
+    def compute_forward(self, batch, stage):
+        """Computation pipeline based on a encoder + emotion classifier."""
+
+        batch = batch.to(self.device)
+        wavs, wav_lens = batch.sig
+        in_toks, _ = batch.speech_tokens
+
+        in_embs = self.modules.discrete_embedding_layer(
+            in_toks
+        )  # [B, T, N-Q, D]
+
+        # Attention-Pooling
+        att_w = self.modules.attention_mlp(in_embs)  # [B, T, N-Q, 1]
+        in_embs = torch.matmul(att_w.transpose(2, -1), in_embs).squeeze(
+            -2
+        )  # [B, T, D]
+
+        # forward modules
+        if "encoder"  in self.modules and type(self.modules.encoder).__name__ == "Sequential":
+            enc_out = self.modules.encoder(in_embs)
+
+        else:
+            enc_out = in_embs
+
+        # last dim will be used for AdaptativeAVG pool
+       
+        outputs = self.hparams.avg_pool(enc_out, wav_lens)
+        outputs = outputs.view(outputs.shape[0], -1)
+        outputs = self.modules.classifier(outputs)
+        outputs = self.hparams.log_softmax(outputs)
+        return outputs
+
+    def compute_objectives(self, predictions, batch, stage):
+        """Computes the loss using speaker-id as label."""
+        scenario_id, _ = batch.scenario_encoded
+        scenario_id = scenario_id.squeeze(1)
+        loss = self.hparams.compute_cost(predictions, scenario_id)
+        if stage != sb.Stage.TRAIN:
+            self.error_metrics.append(batch.id, predictions, scenario_id)
+        return loss
+
+    def on_stage_start(self, stage, epoch=None):
+        """Gets called at the beginning of each epoch.
+        Arguments
+        ---------
+        stage : sb.Stage
+            One of sb.Stage.TRAIN, sb.Stage.VALID, or sb.Stage.TEST.
+        epoch : int
+            The currently-starting epoch. This is passed
+            `None` during the test stage.
+        """
+
+        # Set up statistics trackers for this stage
+        self.loss_metric = sb.utils.metric_stats.MetricStats(
+            metric=sb.nnet.losses.nll_loss
+        )
+
+        # Set up evaluation-only statistics trackers
+        if stage != sb.Stage.TRAIN:
+            self.error_metrics = self.hparams.error_stats()
+
+    def on_stage_end(self, stage, stage_loss, epoch=None):
+        """Gets called at the end of an epoch.
+        Arguments
+        ---------
+        stage : sb.Stage
+            One of sb.Stage.TRAIN, sb.Stage.VALID, sb.Stage.TEST
+        stage_loss : float
+            The average loss for all of the data processed in this stage.
+        epoch : int
+            The currently-starting epoch. This is passed
+            `None` during the test stage.
+        """
+
+        # Store the train loss until the validation stage.
+        if stage == sb.Stage.TRAIN:
+            self.train_loss = stage_loss
+
+        # Summarize the statistics from the stage for record-keeping.
+        else:
+            stats = {
+                "loss": stage_loss,
+                "error_rate": self.error_metrics.summarize("average"),
+            }
+
+        # At the end of validation...
+        if stage == sb.Stage.VALID:
+            if type(self.hparams.scheduler).__name__ == "NewBobScheduler":
+                lr, new_lr = self.hparams.scheduler( stats["error_rate"])
+                sb.nnet.schedulers.update_learning_rate(self.optimizer, new_lr)
+            elif type(self.hparams.scheduler).__name__ == "LinearNoamScheduler":
+                lr = self.hparams.scheduler.current_lr
+            else:
+                raise NotImplementedError
+
+            optimizer = self.optimizer.__class__.__name__
+
+            # The train_logger writes a summary to stdout and to the logfile.
+            epoch_stats = {
+                "epoch": epoch,
+                "lr": lr,
+                "optimizer": optimizer,
+            }
+            self.hparams.train_logger.log_stats(
+                stats_meta=epoch_stats,
+                train_stats={"loss": self.train_loss},
+                valid_stats=stats,
+            )
+
+            # Save the current checkpoint and delete previous checkpoints,
+            self.checkpointer.save_and_keep_only(
+                meta=stats, min_keys=["error_rate"]
+            )
+
+        # We also write statistics about test data to stdout and to logfile.
+        if stage == sb.Stage.TEST:
+            self.hparams.train_logger.log_stats(
+                {"Epoch loaded": self.hparams.epoch_counter.current},
+                test_stats=stats,
+            )
+
+def dataio_prep(hparams):
+    """This function prepares the datasets to be used in the brain class.
+    It also defines the data processing pipeline through user-defined
+    functions. We expect `prepare_mini_librispeech` to have been called before
+    this, so that the `train.json`, `valid.json`,  and `valid.json` manifest
+    files are available.
+    Arguments
+    ---------
+    hparams : dict
+        This dictionary is loaded from the `train.yaml` file, and it includes
+        all the hyperparameters needed for dataset construction and loading.
+    Returns
+    -------
+    datasets : dict
+        Contains two keys, "train" and "valid" that correspond
+        to the appropriate DynamicItemDataset object.
+    """
+    data_folder = hparams["data_folder"]
+
+    train_data = sb.dataio.dataset.DynamicItemDataset.from_csv(
+        csv_path=hparams["csv_train"], replacements={"data_root": data_folder},
+    )
+
+    if hparams["sorting"] == "ascending":
+        # we sort training data to speed up training and get better results.
+        train_data = train_data.filtered_sorted(sort_key="duration")
+        # when sorting do not shuffle in dataloader ! otherwise is pointless
+        hparams["dataloader_opts"]["shuffle"] = False
+
+    elif hparams["sorting"] == "descending":
+        train_data = train_data.filtered_sorted(
+            sort_key="duration", reverse=True
+        )
+        # when sorting do not shuffle in dataloader ! otherwise is pointless
+        hparams["dataloader_opts"]["shuffle"] = False
+
+    elif hparams["sorting"] == "random":
+        pass
+
+    else:
+        raise NotImplementedError(
+            "sorting must be random, ascending or descending"
+        )
+
+    valid_data = sb.dataio.dataset.DynamicItemDataset.from_csv(
+        csv_path=hparams["csv_valid"], replacements={"data_root": data_folder},
+    )
+    valid_data = valid_data.filtered_sorted(sort_key="duration")
+
+    test_data = sb.dataio.dataset.DynamicItemDataset.from_csv(
+        csv_path=hparams["csv_test"], replacements={"data_root": data_folder},
+    )
+    test_data = test_data.filtered_sorted(sort_key="duration")
+
+    datasets = [train_data, valid_data, test_data]
+
+    # Define audio pipeline
+    @sb.utils.data_pipeline.takes("wav")
+    @sb.utils.data_pipeline.provides("sig")
+    def audio_pipeline(wav):
+        """Load the signal, and pass it and its length to the corruption class.
+        This is done on the CPU in the `collate_fn`."""
+        sig = sb.dataio.dataio.read_audio(wav)
+        info = torchaudio.info(wav)
+        resampled = torchaudio.transforms.Resample(
+            info.sample_rate, hparams["sample_rate"],
+        )(sig)
+        #         resampled = resampled.unsqueeze(0)
+        return resampled
+    sb.dataio.dataset.add_dynamic_item(datasets, audio_pipeline)
+    # ]Define tokens pipeline:
+    tokens_loader = hparams["tokens_loader"]
+    num_codebooks = hparams["num_codebooks"]
+
+    @sb.utils.data_pipeline.takes("id")
+    @sb.utils.data_pipeline.provides("speech_tokens")
+    def tokens_pipeline(id):
+        tokens = tokens_loader.tokens_by_uttid(id, num_codebooks=num_codebooks)
+        return tokens
+    sb.dataio.dataset.add_dynamic_item(datasets, tokens_pipeline)
+
+    # Initialization of the label encoder. The label encoder assignes to each
+    # of the observed label a unique index (e.g, 'spk01': 0, 'spk02': 1, ..)
+    label_encoder = sb.dataio.encoder.CategoricalEncoder()
+
+    # Define label pipeline:
+    @sb.utils.data_pipeline.takes("semantics")
+    @sb.utils.data_pipeline.provides("scenario", "scenario_encoded")
+    def label_pipeline(semantics):
+        scenario = semantics.split("'")[3]
+        yield scenario
+        scenario_encoded = label_encoder.encode_label_torch(scenario)
+        yield scenario_encoded
+
+    sb.dataio.dataset.add_dynamic_item(datasets, label_pipeline)
+    # Define datasets. We also connect the dataset with the data processing
+    # functions defined above.
+    sb.dataio.dataset.set_output_keys(
+        datasets, ["id", "sig", "scenario", "scenario_encoded" , "speech_tokens"],
+    )
+    # Load or compute the label encoder (with multi-GPU DDP support)
+    # Please, take a look into the lab_enc_file to see the label to index
+    # mappinng.
+
+    lab_enc_file = os.path.join(hparams["save_folder"], "label_encoder.txt")
+    label_encoder.load_or_create(
+        path=lab_enc_file, from_didatasets=[datasets[0]], output_key="scenario",
+    )
+
+    return {"train": datasets[0], "valid": datasets[1], "test": datasets[2]}
+
+
+# RECIPE BEGINS!
+if __name__ == "__main__":
+    # Reading command line arguments.
+    hparams_file, run_opts, overrides = sb.parse_arguments(sys.argv[1:])
+
+    # Initialize ddp (useful only for multi-GPU DDP training).
+    sb.utils.distributed.ddp_init_group(run_opts)
+
+    # Load hyperparameters file with command-line overrides.
+    with open(hparams_file) as fin:
+        hparams = load_hyperpyyaml(fin, overrides)
+
+    # Create experiment directory
+    sb.create_experiment_directory(
+        experiment_directory=hparams["output_folder"],
+        hyperparams_to_save=hparams_file,
+        overrides=overrides,
+    )
+
+    if hparams["discrete_embedding_layer"].init:
+        hparams["discrete_embedding_layer"].init_embedding(
+            hparams["codec"]
+            .vocabulary[: hparams["num_codebooks"], :, :]
+            .flatten(0, 1)
+        )
+    from slurp_prepare import prepare_SLURP  # noqa
+
+    # multi-gpu (ddp) save data preparation
+    run_on_main(
+        prepare_SLURP,
+        kwargs={
+            "data_folder": hparams["data_folder"],
+            "save_folder": hparams["cached_data_folder"],
+            "train_splits": hparams["train_splits"],
+            "slu_type": "direct",
+            "skip_prep": hparams["skip_prep"],
+        },
+    )
+
+    # Data preparation, to be run on only one process.
+    # Create dataset objects "train", "valid", and "test".
+    datasets = dataio_prep(hparams)
+
+
+    # Use pretrained embeddings
+    if hparams["pretrain_embeddings"]:
+        tokens_loader = hparams["tokens_loader"]
+        embs = tokens_loader.load_pretrained_embeddings(
+            hparams["pretain_embeddings_folder"]
+        )
+        if isinstance(hparams["num_codebooks"], int):
+            embs = embs[
+                : hparams["num_codebooks"] * hparams["vocab_size"],
+            ]
+        # For discrete SSL, num_codebooks is a list used to determine which layers to use.
+        # It is not sequential and can be, for example, [0, 1] or [1, 4].
+        elif isinstance(hparams["num_codebooks"], list):
+            indices = [
+                i
+                for codebook_idx in hparams["num_codebooks"]
+                for i in range(
+                    codebook_idx * hparams["vocab_size"],
+                    (codebook_idx + 1) * hparams["vocab_size"],
+                )
+            ]
+            indices = torch.tensor(indices, dtype=torch.long)
+            embs = embs[indices]
+        hparams["discrete_embedding_layer"].init_embedding(embs)
+
+    # Log number of parameters/buffers
+    model_params = sum(
+        [
+            x.numel()
+            for module in hparams["modules"].values()
+            for x in module.state_dict().values()
+        ]
+    )
+    hparams["train_logger"].log_stats(
+        stats_meta={
+            "Model parameters/buffers (M)": f"{model_params / 1e6:.2f}",
+        },
+    )
+
+    # Initialize the Brain object to prepare for mask training.
+    ic_id_brain = IntentIdBrain(
+        modules=hparams["modules"],
+        opt_class=hparams["model_opt_class"],
+        hparams=hparams,
+        run_opts=run_opts,
+        checkpointer=hparams["checkpointer"],
+    )
+
+    # The `fit()` method iterates the training loop, calling the methods
+    # necessary to update the parameters of the model. Since all objects
+    # with changing state are managed by the Checkpointer, training can be
+    # stopped at any point, and will be resumed on next call.
+        # Measure time
+    start_time = time.time()  # Start the timer
+    ic_id_brain.fit(
+        epoch_counter=ic_id_brain.hparams.epoch_counter,
+        train_set=datasets["train"],
+        valid_set=datasets["valid"],
+        train_loader_kwargs=hparams["train_dataloader_opts"],
+        valid_loader_kwargs=hparams["valid_dataloader_opts"],
+    )
+    end_time = time.time()  # End the timer
+    # Calculate elapsed time
+    elapsed_time = end_time - start_time
+    logger.info(f"Model execution time: {elapsed_time:.6f} seconds")
+
+    if hparams["testing"]:
+    # Testing
+        # Load the best checkpoint for evaluation
+        test_stats = ic_id_brain.evaluate(
+            test_set=datasets["test"],
+            min_key="error_rate",
+            test_loader_kwargs=hparams["test_dataloader_opts"],
+        )
