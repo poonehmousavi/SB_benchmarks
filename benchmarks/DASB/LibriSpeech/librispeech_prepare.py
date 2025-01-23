3c29e855a972fd6c3f6182c0861328f1483c2b74
poonehmousavi <mousavi.pooneh@gmail.com>
2025-01-23 21:03:01 +0000

    fix data_loor of librispeech

--- benchmarks/DASB/LibriSpeech/librispeech_prepare.py
+++ benchmarks/DASB/LibriSpeech/librispeech_prepare.py
@@ -13,6 +13,7 @@ Author
 """
 
 import os
+import re
 import csv
 import random
 from collections import Counter
@@ -291,6 +292,7 @@ def process_line(wav_file, text_dict) -> LSRow:
 
     info = read_audio_info(wav_file)
     duration = info.num_frames / info.sample_rate
+    wav_file = re.sub(r'^.*(?=LibriSpeech)', 'data_root/', wav_file)
 
     return LSRow(
         snt_id=snt_id,
