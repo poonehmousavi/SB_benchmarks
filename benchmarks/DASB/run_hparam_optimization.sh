d094aafacb569e977cb01c4d3b4243f58de97886
poonehmousavi <moosavi.pooneh@gmail.com>
2025-01-30 12:14:41 -0500

    fix resume final runs

--- benchmarks/DASB/run_hparam_optimization.sh
+++ benchmarks/DASB/run_hparam_optimization.sh
@@ -415,6 +415,6 @@ scp $best_yaml_file $final_yaml_file
 ./run_experiments.sh --hparams $final_yaml_file --data_folder $data_folder  --cached_data_folder $cached_data_folder \
   --output_folder $output_folder/best --task $task   --dataset $dataset  --seed $seed\
   --nruns $nruns_eval --eval_metric $eval_metric --eval_set test \
-  --rnd_dir $store_all --testing True $additional_flags
+  --rnd_dir False --testing True $additional_flags
 
 echo "The test performance with best hparams is available at  $output_folder/best"
