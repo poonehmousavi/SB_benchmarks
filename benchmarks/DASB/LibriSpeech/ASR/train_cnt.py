9049bf5d36e3198cbac548914168b9d9b0dde0e6
poonehmousavi <moosavi.pooneh@gmail.com>
2025-04-25 09:04:49 -0400

    update checkpoint

--- benchmarks/DASB/LibriSpeech/ASR/train_cnt.py
+++ benchmarks/DASB/LibriSpeech/ASR/train_cnt.py
@@ -223,8 +223,8 @@ class ASR(sb.Brain):
             )
             self.checkpointer.save_and_keep_only(
                 meta={"ACC": stage_stats["ACC"], "epoch": epoch},
-                min_keys=["ACC"],
-                num_to_keep=self.hparams.avg_checkpoints,
+                max_keys=["ACC"],
+                num_to_keep=1,
             )
 
         elif stage == sb.Stage.TEST:
@@ -237,6 +237,11 @@ class ASR(sb.Brain):
                     self.hparams.output_wer_folder, "w", encoding="utf-8"
                 ) as w:
                     self.wer_metric.write_stats(w)
+                self.checkpointer.save_and_keep_only(
+                    meta={"ACC": 1.1, "epoch": epoch},
+                    max_keys=["ACC"],
+                    num_to_keep=1,
+                )
 
     def on_fit_batch_end(self, batch, outputs, loss, should_step):
         if (
@@ -245,7 +250,21 @@ class ASR(sb.Brain):
         ):
             self.hparams.scheduler(self.optimizer)
 
+    # def on_evaluate_start(self, max_key=None, min_key=None):
+    #     """perform checkpoint average if needed"""
+    #     super().on_evaluate_start()
 
+    #     ckpts = self.checkpointer.find_checkpoints(
+    #         max_key=max_key, min_key=min_key
+    #     )
+    #     ckpt = sb.utils.checkpoints.average_checkpoints(
+    #         ckpts, recoverable_name="model"
+    #     )
+
+    #     self.hparams.model.load_state_dict(ckpt, strict=True)
+    #     self.hparams.model.eval()
+    #     print("Loaded the average")
+        
 def dataio_prepare(hparams, tokenizer):
     """This function prepares the datasets to be used in the brain class.
     It also defines the data processing pipeline through user-defined functions.
@@ -530,5 +549,5 @@ if __name__ == "__main__":
             asr_brain.evaluate(
                 test_datasets[k],
                 test_loader_kwargs=hparams["test_dataloader_opts"],
-                min_key="ACC",
+                max_key="ACC",
             )
